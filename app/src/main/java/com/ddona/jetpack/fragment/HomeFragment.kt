5a322c57b89bc18ee92e8f327053b2cbf0e460e5
DoanPT1 <doanpt93cntt@gmail.com>
2021-12-28 19:30:05 +0700

    create destination and navigate to other fragment

--- app/src/main/java/com/ddona/jetpack/fragment/HomeFragment.kt
+++ app/src/main/java/com/ddona/jetpack/fragment/HomeFragment.kt
@@ -1,7 +1,25 @@
 package com.ddona.jetpack.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.ddona.jetpack.R
+import androidx.navigation.fragment.findNavController
+import com.ddona.jetpack.databinding.FragmentHomeBinding
 
-class HomeFragment: Fragment(R.layout.fragment_home) {
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater)
+        binding.btnLogin.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToLoginFragment()
+            findNavController().navigate(action)
+        }
+        return binding.root
+    }
 }
