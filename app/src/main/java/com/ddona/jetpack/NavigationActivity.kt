f505491bc564076b0e77036ff9aa622610d0893f
DoanPT1 <doanpt93cntt@gmail.com>
2021-12-28 20:25:01 +0700

    Setup actionbar with navigation

--- app/src/main/java/com/ddona/jetpack/NavigationActivity.kt
+++ app/src/main/java/com/ddona/jetpack/NavigationActivity.kt
@@ -2,10 +2,31 @@ package com.ddona.jetpack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.ddona.jetpack.databinding.ActivityNavigationBinding
 
 class NavigationActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityNavigationBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_navigation)
+        binding = ActivityNavigationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager
+                .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
